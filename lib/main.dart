bec297ae311932dd6319d10db91372fa9a04c6e0
Cesar Morales <cesar.aml1891@gmail.com>
2023-06-10 17:32:48 -0500

    Acabando configuracion inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tf11p_0030_codigo_firetask/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
