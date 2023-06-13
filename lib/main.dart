b8564605d3e00a2b0bbb7eecdf99b9717855a900
Cesar Morales <cesar.aml1891@gmail.com>
2023-06-13 16:50:52 -0500

    Acabando

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tf11p_0030_codigo_firetask/pages/home_page.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  CollectionReference tasksReference =
+      FirebaseFirestore.instance.collection("tasks");
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase Firestore"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.get().then((QuerySnapshot value) {
+                  // QuerySnapshot collection = value;
+                  // List<QueryDocumentSnapshot> docs = collection.docs;
+                  // QueryDocumentSnapshot doc = docs[0];
+                  // print(doc.id);
+                  // print(doc.data());
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach((QueryDocumentSnapshot element) {
+                    print(element.id);
+                    print(element.data());
+                    //Convirtiendo objeto a Map<String, dynamic>
+                    Map<String, dynamic> myMap =
+                        element.data() as Map<String, dynamic>;
+                    print(myMap["title"]);
+                  });
+                });
+              },
+              child: Text("Obtener la data"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
