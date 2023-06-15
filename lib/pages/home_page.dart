986835d5320af53fe331549f61f7bc43b55b69f3
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-06-15 16:20:56 -0500

    Acabando

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,103 +5,52 @@ class HomePage extends StatelessWidget {
   CollectionReference tasksReference =
       FirebaseFirestore.instance.collection("tasks");
 
+  Stream<int> counter() async* {
+    for (var i = 0; i < 10; i++) {
+      yield i;
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
+  Future<int> getNumber() async {
+    return 1000;
+  }
+
   @override
   Widget build(BuildContext context) {
+    counter().listen((event) {
+      print(event);
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Firebase Firestore"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                tasksReference.get().then((QuerySnapshot value) {
-                  // QuerySnapshot collection = value;
-                  // List<QueryDocumentSnapshot> docs = collection.docs;
-                  // QueryDocumentSnapshot doc = docs[0];
-                  // print(doc.id);
-                  // print(doc.data());
-                  QuerySnapshot collection = value;
-                  collection.docs.forEach((QueryDocumentSnapshot element) {
-                    print(element.id);
-                    print(element.data());
-                    //Convirtiendo objeto a Map<String, dynamic>
-                    Map<String, dynamic> myMap =
-                        element.data() as Map<String, dynamic>;
-                    print(myMap["title"]);
-                  });
-                });
-              },
-              child: Text("Obtener la data"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                tasksReference.add({
-                  "title": "Ir de compras al super 2",
-                  "description": "Debemos comprar comida pra el mes",
-                }).then((DocumentReference value){
-                  print(value.id);
-                }).catchError((error){
-                  print("Ocurrio un error en el registro");
-                }).whenComplete((){
-                  print("Registro terminado");
-                });
-              },
+      body: StreamBuilder(
+        stream: counter(),
+        builder: (BuildContext conext, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            int data = snap.data;
+            return Center(
               child: Text(
-                "Agregar docuemnto",
+                data.toString(),
+                style: TextStyle(
+                  fontSize: 50,
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                tasksReference.doc("8QGBfySyRKdsZSRiUn5V").update({
-                  "title": "Ir de compras al super 222",
-                  "description": "Debemos comprar comida pra el mes 222",
-                }).catchError((error){
-                  print("Ocurrio un error en el registro");
-                }).whenComplete((){
-                  print("Registro actualizado");
-                });
-              },
-              child: Text(
-                "Actualizar docuemnto",
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                tasksReference.doc("8QGBfySyRKdsZSRiUn5V")
-                .delete()
-                .catchError((error){
-                  print("Ocurrio un error en el registro");
-                }).whenComplete((){
-                  print("Eliminacion actualizado");
-                });
-              },
-              child: Text(
-                "Eliminar docuemnto",
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                tasksReference.doc("A0001")
-                .set({
-                  "title": "Ir de compras al super 333",
-                  "description": "Debemos comprar comida pra el mes 333",
-                })
-                .catchError((error){
-                  print("Ocurrio un error en el registro");
-                }).whenComplete((){
-                  print("Creacion completada");
-                });
-              },
-              child: Text(
-                "Agregar docuemnto personalizado",
-              ),
-            ),
-          ],
-        ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
+      // body: FutureBuilder(
+      //   future: getNumber(),
+      //   builder: (BuildContext conext, AsyncSnapshot snap){
+      //     return Text(snap.data.toString());
+      //   },
+      // ),
     );
   }
 }
