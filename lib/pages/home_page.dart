66cc4380efe27548beb5fe98f1cb28da596a3bdf
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-06-15 16:41:58 -0500

    Acabando

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,52 +5,47 @@ class HomePage extends StatelessWidget {
   CollectionReference tasksReference =
       FirebaseFirestore.instance.collection("tasks");
 
-  Stream<int> counter() async* {
-    for (var i = 0; i < 10; i++) {
-      yield i;
-      await Future.delayed(const Duration(seconds: 2));
-    }
-  }
-
-  Future<int> getNumber() async {
-    return 1000;
-  }
-
   @override
   Widget build(BuildContext context) {
-    counter().listen((event) {
-      print(event);
-    });
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Firestore"),
-      ),
-      body: StreamBuilder(
-        stream: counter(),
-        builder: (BuildContext conext, AsyncSnapshot snap) {
-          if (snap.hasData) {
-            int data = snap.data;
+        appBar: AppBar(
+          title: Text("Firebase Firestore"),
+        ),
+        body: StreamBuilder(
+          stream: tasksReference.snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snap) {
+            if (snap.hasData) {
+              QuerySnapshot collection = snap.data;
+              List<QueryDocumentSnapshot> docs = collection.docs;
+              List<Map<String, dynamic>> docsMap =
+                  docs.map((e) => e.data() as Map<String, dynamic>).toList();
+
+              return ListView.builder(
+                itemCount: docsMap.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(docsMap[index]["title"]),
+                  );
+                },
+              );
+              // return ListView.builder(
+              //   itemCount: docsMap.length,
+              //   itemBuilder: (BuildContext context, int index){
+              //     return Text(docsMap[index]["title"]);
+              //   },
+              // );
+            }
             return Center(
-              child: Text(
-                data.toString(),
-                style: TextStyle(
-                  fontSize: 50,
-                ),
-              ),
+              child: CircularProgressIndicator(),
             );
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-      // body: FutureBuilder(
-      //   future: getNumber(),
-      //   builder: (BuildContext conext, AsyncSnapshot snap){
-      //     return Text(snap.data.toString());
-      //   },
-      // ),
-    );
+          },
+        )
+        // body: FutureBuilder(
+        //   future: getNumber(),
+        //   builder: (BuildContext conext, AsyncSnapshot snap){
+        //     return Text(snap.data.toString());
+        //   },
+        // ),
+        );
   }
 }
